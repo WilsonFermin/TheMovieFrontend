e26eab7642d07a2377a7ab623e0e204a92a9aac9
wilsonruiz <wruizfermin@gmail.com>
2022-08-04 22:09:28 -0600

    Maquetacion, navegacion por Home, Treding and Category

--- src/main.js
+++ src/main.js
@@ -1,3 +1,51 @@
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+    },
+    params: {
+        'api_key': API_KEY,
+    },
+});
 
+async function getTredingMoviesPreview(){
+    const { data } = await api('trending/movie/day');
+    const movies = data.results;
 
-console.log("Heyyy!!" + API_KEY)
+    trendingMoviesPreviewList.innerHTML = "";
+
+    movies.forEach(movie => {
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
+
+        movieContainer.appendChild(movieImg);
+        trendingMoviesPreviewList.appendChild(movieContainer);
+    });
+}
+
+async function getCategoriesPreview(){
+    const { data } = await api('genre/movie/list');
+    const categories = data.genres;
+
+    categoriesPreviewList.innerHTML = "";
+
+    categories.forEach(category => {      
+      const categoryContainer = document.createElement('div');
+      categoryContainer.classList.add('category-container');
+  
+      const categoryTitle = document.createElement('h3');
+      categoryTitle.classList.add('category-title');
+      categoryTitle.setAttribute('id', 'id' + category.id);
+      const categoryTitleText = document.createTextNode(category.name);
+  
+      categoryTitle.appendChild(categoryTitleText);
+      categoryContainer.appendChild(categoryTitle);
+      categoriesPreviewList.appendChild(categoryContainer);
+    });
+}
+
